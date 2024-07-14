2a6a41380bf89d47d71ea3b0c61aa2a92db39084
gcastrod <gabriel.castro@intel.com>
2024-07-14 13:41:14 -0600

    first commit

---
+++ src/main/java/com/dcplinaWeb/DcplinaWebApplication.java
@@ -0,0 +1,13 @@
+package com.dcplinaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcplinaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcplinaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dcplinaWeb/DcplinaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dcplinaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcplinaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
