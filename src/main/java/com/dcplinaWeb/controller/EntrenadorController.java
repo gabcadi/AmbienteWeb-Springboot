05a2651a7c772dfe983335fd151e1059bba20220
iespinoza421 <espinozamonteroisaac@gmail.com>
2024-08-01 22:12:36 -0600

    EntrenadorController

--- src/main/java/com/dcplinaWeb/controller/EntrenadorController.java
+++ src/main/java/com/dcplinaWeb/controller/EntrenadorController.java
@@ -2,6 +2,7 @@ package com.dcplinaWeb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EntrenadorController {
@@ -10,4 +11,11 @@ public class EntrenadorController {
     public String entrenadorHome() {
         return "entrenador/home";
     }
+    
+    
+    @GetMapping("/entrenador/inbox")
+    public String entrenadorInbox() {
+        return "entrenador/inbox";
+    }
 }
+
